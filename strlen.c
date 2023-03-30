fc11de9d09136bf3427ffb05ad30f9d1a9ab0577
Thaynara Fonseca <tatafonseca15@gmail.com>
2023-03-30 09:23:41 -0300

    Create strlen.c

---
+++ strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char stringA[] = "PROGRAMACAO C";
+    char stringB[] = "COMPUTACAO EM NUVEM";
+    printf("String A: %s\n", stringA);
+    printf("tamanho da string A: %lu\n\n", strlen(stringA));
+    printf("String B: %s\n", stringB);
+    printf("Tamanho da string B: %lu\n\n", strlen(stringB));
+    
+    return 0;
+    
+}
